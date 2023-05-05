c769af76104659b2b1887c601893bd69a4635f11
Saurabh Rajguru <saurabhrajguru@Saurabhs-MacBook-Air.local>
2023-05-06 02:57:52 +0530

    Added script for folder to tmux session

---
+++ bin/.local/scripts/tmux-sessionizer.bash
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# My code below
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+    selected=$(fd . ~ ~/workspace ~/Documents ~/Downloads ~/Desktop --exact-depth 1 --color never --type d | fzf) 
+fi
+
+#selected_base=$(basename $selected | tr . _)
+#
+#if [[ $(tmux list-sessions | grep $selected_base -c) -eq 0 ]]; then
+#    tmux new -ds $selected_base -c $selected
+#fi
+#
+#tmux switch-client -t $selected_base
+
+################################################################################
+#Primeagen code below: https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/scripts/tmux-sessionizer
+
+if [[ -z $selected ]]; then
+        exit 0
+fi
+
+selected_name=$(basename "$selected" | tr . _)
+tmux_running=$(pgrep tmux)
+
+# [[true]] is exit status 0 , TMUX is set when inside tmux session.
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+        tmux new-session -s $selected_name -c $selected
+            exit 0
+fi
+
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
+        tmux new-session -ds $selected_name -c $selected
+fi
+
+tmux switch-client -t $selected_name
