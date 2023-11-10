61bebbd8338dad4cbaf786fd641dff622cb812cc
Saurabh Rajguru <saurabhrajguru7@gmail.com>
2023-11-10 03:53:47 -0500

    feat: venv arg for pyenv script

--- bin/.local/scripts/init-pyenv.bash
+++ bin/.local/scripts/init-pyenv.bash
@@ -1,16 +1,22 @@
 #!/usr/bin/bash
 
-read -r -p "ensure pyenv global is set to desired python version
+read -r -p "put custom directory name as argument to script otherwise
+current directory name will be used as the virtual env name
+ensure pyenv global is set to desired python version
 current version: $(pyenv global)
 ensure you are in correct working directory
 PWD: $PWD
 continue? [y/N] " response
 
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    basename "$PWD" | xargs -I {} pyenv virtualenv "{}"
-    pyenv local "$(pyenv global)/envs/$(basename $PWD)"
+     if [ -z "$1" ]; then
+        $1=$(basename $PWD)
+     fi
 
-    read -r -p "proceed with pip-tools install? [y/N]" response
+    pyenv virtualenv "$1"
+    pyenv local "$(pyenv global)/envs/$1"
+
+    read -r -p "proceed with pip-tools install? [y/N] " response
 
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
         pip install pip-tools
