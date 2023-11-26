404a8f9d6045cd11e54e60af8f57dcdae1b514ff
Saurabh Rajguru <saurabhrajguru7@gmail.com>
2023-11-26 02:04:34 -0500

    update: add rough in backup

--- bin/.local/scripts/wsl-backupdisk.bash
+++ bin/.local/scripts/wsl-backupdisk.bash
@@ -12,20 +12,24 @@ cd $HOME
 if [ "$1" = 'upload' ]; then
     tar cf finance.tar finance/
     tar cf work.tar work/
+    tar cf rough.tar rough/
 
     cp finance.tar /mnt/d/
     cp work.tar /mnt/d/
+    cp rough.tar /mnt/d/
     cp .zsh_history /mnt/d/
-    rm finance.tar work.tar
+    rm finance.tar work.tar rough.tar
 
 elif [ "$1" = 'download' ]; then
     cp /mnt/d/finance.tar ./
     cp /mnt/d/work.tar ./
+    cp /mnt/d/rough.tar ./
     cp /mnt/d/.zsh_history ./
 
     tar xf finance.tar
     tar xf work.tar
-    rm finance.tar work.tar
+    tar xf rough.tar
+    rm finance.tar work.tar rough.tar
 
 else
     echo "wsl-backupdisk.bash upload - upload private data to external ssd"
