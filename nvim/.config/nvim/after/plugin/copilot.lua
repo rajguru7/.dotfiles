933ccc3d341b486585a9864575d40a021e4de088
Saurabh Rajguru <saurabhrajguru7@gmail.com>
2024-11-01 12:40:59 -0400

    disable copilot by default

--- nvim/.config/nvim/after/plugin/copilot.lua
+++ nvim/.config/nvim/after/plugin/copilot.lua
@@ -1,2 +1,4 @@
+vim.cmd("silent! Copilot disable")
 vim.keymap.set('i', '<M-f>', '<Plug>(copilot-accept-word)')
 vim.keymap.set('i', '<C-e>', '<Plug>(copilot-accept-line)')
+vim.api.nvim_set_keymap('n', '<leader>ce', ':Copilot enable<CR>', { noremap = true, silent = true })
